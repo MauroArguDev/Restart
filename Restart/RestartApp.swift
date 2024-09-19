4430b7578665559f85f39b0b632c1b6d3c218008
Mauricio Argumedo <mauargudev@gmail.com>
2024-09-19 12:17:34 -0600

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Mauricio Argumedo on 19/9/24.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
